1c375ab0ce34d9af2be42e108bac89ca34d82bb4
Jacob Barrett <jakewbarrett@gmail.com>
2017-06-15 13:57:56 -0500

    Create steamId.js

--- js/steamId.js
+++ js/steamId.js
@@ -8,6 +8,10 @@
          {
             "steam_id": "STEAM_1:1:11374238",
             "name": "Who Cares!"
+         },
+         {
+            "steam_id": "STEAM_0:0:8845346",
+            "name": "Zpamm"
          }
       ]
    }
