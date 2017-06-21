85feddf41af6b27c76ef829ead483e162ece1883
Jacob Barrett <jakewbarrett@gmail.com>
2017-06-21 15:10:19 -0500

    Update APIGet.js

--- js/APIGet.js
+++ js/APIGet.js
@@ -1,49 +1,29 @@
-var jumpstats_string = "https://kztimerglobal.com/api/v1/jumpstats?";
+var jumpstats_string = "http://kztimerglobal.com/api/v1/jumpstats?";
 var jsonPlayerName = {};
-var jsonPlayerDistance = {};
-
-function get_playersLJPB() {
-
-  var url_string = jumpstats_string;
-  var playerName = "";
-
-  if($("#steam_id").val().length > 0) {
-    steam_id = encodeURIComponent($("#steam_id").val());
-    url_string += "steam_id=" + steam_id + "&" + "jumptype=longjump";
-
-    steam_id_c = steam_id.split("%3A").join(":");
-    playerName = jsonPlayerName[steam_id_c];
-    console.log(playerName);
-
-    $.ajax({
-      type: "GET",
-      url: url_string,
-      success: function(json) {
-        try {
-          if (json[0].distance !== undefined) {
-            $('#APIPost').text(playerName + "'s best longjump is " + json[0].distance);
-          }
-        }
-        catch(err) {
-          $('#APIPost').text("The given steam id is either an invalid steam id or doesn't have a longjump in the database. Message me on Steam and I'll add you to do the steam id database.");
-        }
-      },
-      error: function() {
-        console.log("it failed");
-      }
-    });
-  }
-}
+var jsonPlayerLJDistance = {};
+var playerList = [];
+var playerLongjump;
+var playerBhop;
+var playerMbhop;
+var table = document.getElementById("myTable");
 
 $(document).ready(function(){
+  LoadPlayers();
 
   $.ajax({
-    url: "https://zach47.github.io/js/steamId.js",
+    url: "http://jacobwbarrett.com/js/steamId.js",
     success: function(player) {
       data = JSON.parse(player);
-
       data[0].players.forEach(function (playerInfo) {
         jsonPlayerName[playerInfo.steam_id] = playerInfo.name;
       });
     }});
+
+  if (table.rows.length === 20) {
+    console.log("full");
+  }
+
+    setTimeout(function(){
+      $(".se-pre-con").fadeOut("slow");;
+    },1000);
   });
