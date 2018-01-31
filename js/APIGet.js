d2206215f07b1878bf6440cb8da4cf463917e147
Jacob Barrett <jakewbarrett@gmail.com>
2018-01-31 02:29:38 -0600

    Update APIGet.js

--- js/APIGet.js
+++ js/APIGet.js
@@ -1,79 +1,60 @@
 // Arrays
-var jsonPlayerName = {};
-var playerListLJ = [];
-var playerListBHOP = [];
-var playerListMBHOP = [];
-var playerListWJ = [];
-var playerListDBHOP = [];
-var playerListCJ = [];
-var playerListLAJ = [];
+let playerList = [];
+let TableSelection = "";
 // Links
-var LJurl = "https://staging.kztimerglobal.com/api/v1/jumpstats/longjump/top?is_crouch_boost=false&limit=2000";
-var BHOPurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=bhop&is_crouch_boost=false&limit=500";
-var MBHOPurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=multibhop&is_crouch_boost=false&limit=500";
-var WJurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=weirdjump&is_crouch_boost=false&limit=500";
-var DBHOPurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=dropbhop&is_crouch_boost=false&limit=500";
-var CJurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=countjump&is_crouch_boost=false&limit=500";
-var LAJurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=ladderjump&is_crouch_boost=false&limit=500";
+let LJurl = "https://staging.kztimerglobal.com/api/v1/jumpstats/longjump/top?is_crouch_boost=false&limit=2000";
+let BHOPurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=bhop&is_crouch_boost=false&limit=500";
+let MBHOPurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=multibhop&is_crouch_boost=false&limit=500";
+let WJurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=weirdjump&is_crouch_boost=false&limit=500";
+let DBHOPurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=dropbhop&is_crouch_boost=false&limit=500";
+let CJurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=countjump&is_crouch_boost=false&limit=500";
+let LAJurl = "https://staging.kztimerglobal.com/api/v1/jumpstats?jumptype=ladderjump&is_crouch_boost=false&limit=500";
 // Steam
-var steam64;
-var steamBigInt;
-var playerName;
+let steam64;
+let steamBigInt;
+let playerName;
 
 
 $(document).ready(function(){
-  $.ajax({
-    url: "../js/steamId.js",
-    success: function(player) {
-      data = JSON.parse(player);
-      data[0].players.forEach(function (playerInfo) {
-        jsonPlayerName[playerInfo.steam_id] = playerInfo.name;
-      });
-    }});
 
-    $("#jumptype-button-bind").on('touchstart click', function() {
+  document.addEventListener('click', (event) => {
+    if (event.target.id === "jumptype-button-bind") {
       Bind();
-    })
-    $("#jumptype-button-lj").on('touchstart click', function(){
+    }
+    if (event.target.id === "jumptype-button-lj" || event.target.id === "dropdown-lj") {
+      TableSelection = "TableLJ";
+      url = LJurl;
       showLJ();
-    });
-    $("#jumptype-button-bhop").on('touchstart click', function(){
+    }
+    if (event.target.id === "jumptype-button-bhop" || event.target.id === "dropdown-bhop") {
+      TableSelection = "TableBHOP";
+      url = BHOPurl;
       showBHOP();
-    });
-    $("#jumptype-button-mbhop").on('touchstart click', function(){
+    }
+    if (event.target.id === "jumptype-button-mbhop" || event.target.id === "dropdown-mbhop") {
+      TableSelection = "TableMBHOP";
+      url = MBHOPurl;
       showMBHOP();
-    });
-    $("#jumptype-button-wj").on('touchstart click', function(){
+    }
+    if (event.target.id === "jumptype-button-wj" || event.target.id === "dropdown-wj") {
+      TableSelection = "TableWJ";
+      url = WJurl;
       showWJ();
-    });
-    $("#jumptype-button-dbhop").on('touchstart click', function(){
+    }
+    if (event.target.id === "jumptype-button-dbhop" || event.target.id === "dropdown-dbhop") {
+      TableSelection = "TableDBHOP";
+      url = DBHOPurl;
       showDBHOP();
-    });
-    $("#jumptype-button-cj").on('touchstart click', function(){
+    }
+    if (event.target.id === "jumptype-button-cj" || event.target.id === "dropdown-cj") {
+      TableSelection = "TableCJ";
+      url = CJurl;
       showCJ();
-    });
-    $("#jumptype-button-laj").on('touchstart click', function(){
+    }
+    if (event.target.id === "jumptype-button-laj" || event.target.id === "dropdown-laj") {
+      TableSelection = "TableLAJ";
+      url = LAJurl;
       showLAJ();
-    });
-    $("#dropdown-lj").on('touchstart click', function(){
-      showLJ();
-    });
-    $("#dropdown-bhop").on('touchstart click', function(){
-      showBHOP();
-    });
-    $("#dropdown-mbhop").on('touchstart click', function(){
-      showMBHOP();
-    });
-    $("#dropdown-wj").on('touchstart click', function(){
-      showWJ();
-    });
-    $("#dropdown-dbhop").on('touchstart click', function(){
-      showDBHOP();
-    });
-    $("#dropdown-cj").on('touchstart click', function(){
-      showCJ();
-    });
-    $("#dropdown-laj").on('touchstart click', function(){
-      showLAJ();
-    });
+    }
   });
+});
